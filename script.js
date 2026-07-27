44724defd1a184122a5ebf1999f1877f3ed7895f
Kacper Chwiedor <kacperchwiedor@gmail.com>
2026-07-27 18:37:06 +0200

    adding firebase database v2

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-<script type="module">
+
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAO6W4-rLOYqVyrcHdbKlMd6BZAAqYDWQI",
     authDomain: "cudodomki.firebaseapp.com",
     projectId: "cudodomki",
@@ -15,12 +15,12 @@
     messagingSenderId: "854596007648",
     appId: "1:854596007648:web:5c62f53622f81e78814285",
     measurementId: "G-84Z26DF7S6"
-  };
+};
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const toggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.main-nav');
